4c3bef8c09ad9051c21888742b2b26068b1b52c2
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-10-08 13:56:00 -0600

    begin code for war

--- war.lua
+++ war.lua
@@ -2,4 +2,53 @@ require "cards"
 
 local war = {}
 
+local playerAHand = {}
+local playerAWinnings = {}
+local playerBHand = {}
+local playerBWinnings = {}
+
+local currentWinningPlayer = nil
+
+local function setup()
+	local deck = newDeck()
+	shuffle(deck)
+	for i=1,26 do
+		playerAHand[#playerAHand+1]=deck[i]
+	end
+	for i = 27,52 do
+		playerBHand[#playerBHand+1]=deck[i]
+	end
+end
+
+local function gameWon()
+	if f_debug then
+		local total = #playerAHand + #playerAWinnings
+		total = total + #playerBHand + #playerBWinnings
+		if total ~= 52 then
+			error("total card count is not 52, something went wrong")
+		end
+	end
+
+	if #playerAHand + #playerAWinnings == 52 then
+		return true
+	elseif #playerBHand + #playerBWinnings == 52 then
+		return true
+	else
+		return false
+	end
+end
+
+function war.play()
+	setup()
+	local N = 0
+	local T = 0
+	local L = 0
+	
+	repeat
+		N = N + 1
+	until gameWon()
+	
+	print("OUTPUT war turns "..N.." transitions "..T.." last "..(L/N))
+end
+
 return war
