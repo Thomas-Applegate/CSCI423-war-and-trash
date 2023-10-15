39f47eae48b11e306904fc262a448d8d384f0adc
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-10-15 17:43:08 -0600

    complete setup and skeleton of play function

--- trash.lua
+++ trash.lua
@@ -10,7 +10,22 @@ local playerBArray = {}
 local currentWinningPlayer = nil
 
 local function setup()
-
+	local deck = newDeck()
+	shuffle(deck)
+	
+	for i = 1, 10 do
+		playerAArray[i] = deck[#deck]
+		deck[#deck] = nil
+	end
+	for i = 1, 10 do
+		playerABrray[i] = deck[#deck]
+		deck[#deck] = nil
+	end
+	discardPile[1] = deck[#deck]
+	deck[#deck] = nil
+	for _, v in ipairs(deck) do
+		drawPile[#drawPile+1] = v
+	end
 end
 
 local function checkForTransition()
@@ -31,12 +46,21 @@ function trash.play()
 	local T = 0
 	local L = 0
 	
+	local currentArr = playerAArray
 	repeat
 		N = N + 1
+		
+		playTurn(currentArr)
 		
 		if checkForTransition() then
 			T = T + 1
 			L = N
+		end
+		
+		if currentArr == playerAArray then
+			currentArr = playerBArray
+		else
+			currentArr = playerAArray
 		end
 	until gameWon()
 	
