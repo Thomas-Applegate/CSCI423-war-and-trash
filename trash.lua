9214c7dd94f4c9e4bb7239b7e586b35dcbdb49db
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-10-15 18:23:26 -0600

    misc additions

--- trash.lua
+++ trash.lua
@@ -6,6 +6,8 @@ local drawPile = {}
 local discardPile = {}
 local playerAArray = {}
 local playerBArray = {}
+local playerAFaceUp = {}
+local playerBFaceUp = {}
 
 local currentWinningPlayer = nil
 
@@ -16,10 +18,12 @@ local function setup()
 	for i = 1, 10 do
 		playerAArray[i] = deck[#deck]
 		deck[#deck] = nil
+		playerAFaceUp[i] = false
 	end
 	for i = 1, 10 do
-		playerABrray[i] = deck[#deck]
+		playerBArray[i] = deck[#deck]
 		deck[#deck] = nil
+		playerBFaceUp[i] = false
 	end
 	discardPile[1] = deck[#deck]
 	deck[#deck] = nil
@@ -29,14 +33,28 @@ local function setup()
 end
 
 local function checkForTransition()
-	return false --for now until this logic is implimented
+	local lastWinning = currentWinningPlayer
+	if #playerAArray < #playerBArray then --player A is winning
+		currentWinningPlayer = "A"
+	elseif #playerBArray < #playerAArray then --player B is winning
+		currentWinningPlayer = "B"
+	end
+	if lastWinning ~= currentWinningPlayer then
+		return true
+	else
+		return false
+	end
 end
 
 local function gameWon()
-	return true --for now so we don't get an infinite loop
+	if #playerAArray == 0 or #playerBArray == 0 then
+		return true
+	else
+		return false
+	end
 end
 
-local function playTurn(arr)
+local function playTurn(arr, faceUp)
 
 end
 
@@ -47,20 +65,31 @@ function trash.play()
 	local L = 0
 	
 	local currentArr = playerAArray
+	local currentFaceUp = playerAFaceUp
 	repeat
 		N = N + 1
 		
-		playTurn(currentArr)
+		if #drawPile == 0 then --if draw pile is depleated
+			local temp = discardPile[#discardPile]
+			discardPile[#discardPile] = nil
+			drawPile = discardPile
+			shuffle(drawPile)
+			discardPile = { temp }
+		end
+		
+		playTurn(currentArr, currentFaceUp)
 		
 		if checkForTransition() then
 			T = T + 1
 			L = N
 		end
 		
-		if currentArr == playerAArray then
+		if currentArr == playerAArray then --swap the current player
 			currentArr = playerBArray
+			currentFaceUp = playerBFaceUp
 		else
 			currentArr = playerAArray
+			currentFaceUp = playerBFaceUp
 		end
 	until gameWon()
 	
